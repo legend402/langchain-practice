2e7781f6217a21667bbb5f052079ca78d7b9848e
hanyj <1689699478@qq.com>
2026-06-07 22:43:07 +0800

    style: 格式化 auth_models.py 缩进

--- src/service/auth/auth_models.py
+++ src/service/auth/auth_models.py
@@ -1,25 +1,35 @@
 from fastapi_fullauth import CreateUserSchema, UserSchema
-from fastapi_fullauth.models.sqlmodel import RefreshTokenMixin, RoleMixin, UserMixin, UserRoleMixin
+from fastapi_fullauth.models.sqlmodel import (
+    RefreshTokenMixin,
+    RoleMixin,
+    UserMixin,
+    UserRoleMixin,
+)
 from pydantic import Field
 from sqlmodel import Relationship
 
 
 class RefreshToken(RefreshTokenMixin, table=True):
-  pass
+    pass
+
 
 class Role(RoleMixin, table=True):
-  pass
+    pass
+
 
 class UserRole(UserRoleMixin, table=True):
-  pass
+    pass
+
 
 class User(UserMixin, table=True):
-  user_name: str = Field(default="", max_length=50, index=True)
-  roles: list[Role] = Relationship(link_model=UserRole)
-  refresh_token: list[RefreshToken] = Relationship()
+    user_name: str = Field(default="", max_length=50, index=True)
+    roles: list[Role] = Relationship(link_model=UserRole)
+    refresh_token: list[RefreshToken] = Relationship()
+
 
 class AppUserSchema(UserSchema):
-  user_name: str = ""
+    user_name: str = ""
+
 
 class AppCreateUserSchema(CreateUserSchema):
-  user_name: str = ""
+    user_name: str = ""
